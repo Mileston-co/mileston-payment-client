82a32c7104d7704a093ec34c0cbd461efa6cc11d
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-04-26 12:37:31 +0100

    feat: added sui wallet provider

---
+++ src/react/components/SuiWalletProvider.tsx
@@ -0,0 +1,22 @@
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Config options for the networks you want to connect to
+const { networkConfig } = createNetworkConfig({
+    localnet: { url: getFullnodeUrl('testnet') },
+    mainnet: { url: getFullnodeUrl('mainnet') },
+});
+const queryClient = new QueryClient();
+
+export default function SuiWalletProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SuiClientProvider networks={networkConfig} defaultNetwork="localnet">
+                <WalletProvider autoConnect={true}>
+                    {children}
+                </WalletProvider>
+            </SuiClientProvider>
+        </QueryClientProvider>
+    );
+}
