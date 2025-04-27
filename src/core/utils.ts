a421fdedd8eb051ec9250c2e1f935a4b2d7b435d
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-04-27 06:31:26 +0100

    completed evm wallet connect

---
+++ src/core/utils.ts
@@ -0,0 +1,105 @@
+import { 
+    avalanche, 
+    avalancheFuji, 
+    base, 
+    baseSepolia, 
+    polygon, 
+    polygonAmoy, 
+    arbitrum, 
+    arbitrumSepolia, 
+    mainnet, 
+    sepolia 
+} from "viem/chains";
+import { 
+    evmType, 
+    env 
+} from "../types";
+
+export const usdtABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_upgradedAddress", "type": "address" }], "name": "deprecate", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "deprecated", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_evilUser", "type": "address" }], "name": "addBlackList", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "upgradedAddress", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "balances", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "maximumFee", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "_totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "unpause", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_maker", "type": "address" }], "name": "getBlackListStatus", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "address" }], "name": "allowed", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "paused", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "who", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pause", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getOwner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "newBasisPoints", "type": "uint256" }, { "name": "newMaxFee", "type": "uint256" }], "name": "setParams", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "amount", "type": "uint256" }], "name": "issue", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "amount", "type": "uint256" }], "name": "redeem", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" }], "name": "allowance", "outputs": [{ "name": "remaining", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "basisPointsRate", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "isBlackListed", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_clearedUser", "type": "address" }], "name": "removeBlackList", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "MAX_UINT", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_blackListedUser", "type": "address" }], "name": "destroyBlackFunds", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "name": "_initialSupply", "type": "uint256" }, { "name": "_name", "type": "string" }, { "name": "_symbol", "type": "string" }, { "name": "_decimals", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "amount", "type": "uint256" }], "name": "Issue", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "amount", "type": "uint256" }], "name": "Redeem", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "newAddress", "type": "address" }], "name": "Deprecate", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "feeBasisPoints", "type": "uint256" }, { "indexed": false, "name": "maxFee", "type": "uint256" }], "name": "Params", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_blackListedUser", "type": "address" }, { "indexed": false, "name": "_balance", "type": "uint256" }], "name": "DestroyedBlackFunds", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_user", "type": "address" }], "name": "AddedBlackList", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "name": "_user", "type": "address" }], "name": "RemovedBlackList", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [], "name": "Pause", "type": "event" }, { "anonymous": false, "inputs": [], "name": "Unpause", "type": "event" }]
+
+export const usdcABI = [
+    {
+        constant: false,
+        inputs: [
+            { name: '_to', type: 'address' },
+            { name: '_value', type: 'uint256' },
+        ],
+        name: 'transfer',
+        outputs: [{ name: '', type: 'bool' }],
+        type: 'function',
+    },
+];
+
+export function getChain(env: env, evm: evmType) {
+    const chains = {
+        avax: {
+            prod: avalanche,
+            test: avalancheFuji
+        },
+        base: {
+            prod: base,
+            test: baseSepolia
+        },
+        pol: {
+            prod: polygon,
+            test: polygonAmoy
+        },
+        arb: {
+            prod: arbitrum,
+            test: arbitrumSepolia
+        },
+        eth: {
+            prod: mainnet,
+            test: sepolia
+        }
+    };
+
+    return chains[evm][env];
+}
+
+
+export function getUsdcEVMContractAddress(env: env, evm: evmType) {
+    const addresses = {
+        avax: {
+            prod: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
+            test: "0x5425890298aed601595a70AB815c96711a31Bc65"
+        },
+        base: {
+            prod: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+            test: "0x036CbD53842c5426634e7929541eC2318f3dCF7e"
+        },
+        pol: {
+            prod: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+            test: "0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582"
+        },
+        arb: {
+            prod: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+            test: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d"
+        },
+        eth: {
+            prod: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            test: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+        }
+    };
+
+    return addresses[evm][env];
+}
+
+export function getUsdtEVMContractAddress(env: env, evm: evmType) {
+    const addresses = {
+        avax: {
+            prod: "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7",
+            test: null
+        },
+        eth: {
+            prod: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+            test: null
+        }
+    };
+
+    if (!(evm in addresses)) {
+        throw new Error(`Unsupported evmType: ${evm}`);
+    }
+
+    return addresses[evm as keyof typeof addresses][env];
+}
--- src/core/walletConnect.ts
+++ src/core/walletConnect.ts
@@ -6,86 +6,15 @@ import {
     encodeFunctionData,
 } from 'viem';
 import 'viem/window';
-import {
-    sepolia,
-    mainnet,
-    avalanche,
-    avalancheFuji,
-    polygon,
-    polygonAmoy,
-    base,
-    baseSepolia,
-    arbitrum,
-    arbitrumSepolia
-} from 'viem/chains';
-import { env, evmType, PayWithWalletConnect } from '../types';
+import { PayWithWalletConnect } from '../types';
+import { 
+    getChain, 
+    getUsdcEVMContractAddress, 
+    usdcABI,
+    usdtABI,
+    getUsdtEVMContractAddress
+} from './utils';
 
-function getChain(env: env, evm: evmType) {
-    const chains = {
-        avax: {
-            prod: avalanche,
-            test: avalancheFuji
-        },
-        base: {
-            prod: base,
-            test: baseSepolia
-        },
-        pol: {
-            prod: polygon,
-            test: polygonAmoy
-        },
-        arb: {
-            prod: arbitrum,
-            test: arbitrumSepolia
-        },
-        eth: {
-            prod: mainnet,
-            test: sepolia
-        }
-    };
-
-    return chains[evm][env];
-}
-
-function getUsdcEVMContractAddress(env: env, evm: evmType) {
-    const addresses = {
-        avax: {
-            prod: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
-            test: "0x5425890298aed601595a70AB815c96711a31Bc65"
-        },
-        base: {
-            prod: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
-            test: "0x036CbD53842c5426634e7929541eC2318f3dCF7e"
-        },
-        pol: {
-            prod: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
-            test: "0x41e94eb019c0762f9bfcf9fb1e58725bfb0e7582"
-        },
-        arb: {
-            prod: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-            test: "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d"
-        },
-        eth: {
-            prod: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-            test: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
-        }
-    };
-
-    return addresses[evm][env];
-}
-
-const USDC_ABI = [
-    {
-        constant: false,
-        inputs: [
-            { name: '_to', type: 'address' },
-            { name: '_value', type: 'uint256' },
-        ],
-        name: 'transfer',
-        outputs: [{ name: '', type: 'bool' }],
-        type: 'function',
-    },
-];
 
 export async function handlePayWithEVMWalletConnect(
     {
@@ -120,9 +49,12 @@ export async function handlePayWithEVMWalletConnect(
         let tokenAddress: `0x${string}` | null = null;
         let decimals = 18; // Default to 18 decimals for native tokens
 
-        if (token === 'USDC' || token === 'USDT') {
+        if (token === 'USDC') {
             tokenAddress = getUsdcEVMContractAddress(env, evm) as `0x${string}`;
-            decimals = 6; // USDC and USDT typically have 6 decimals
+            decimals = 6; // USDC typically has 6 decimals
+        } else if (token === 'USDT') {
+            tokenAddress = getUsdtEVMContractAddress(env, evm) as `0x${string}`;
+            decimals = 6; // USDT typically has 6 decimals
         }
 
         const netAmountToSendInWei = BigInt(netAmountToSend * 10 ** decimals);
@@ -163,14 +95,16 @@ export async function handlePayWithEVMWalletConnect(
         } else if (token === 'USDC' || token === 'USDT') {
             console.log("Encoding data for token transfer...");
 
+            const abi = token === 'USDC' ? usdcABI : usdtABI;
+
             const data1 = encodeFunctionData({
-                abi: USDC_ABI,
+                abi,
                 functionName: 'transfer',
                 args: [recipientAddress, netAmountToSendInWei],
             });
 
             const data2 = encodeFunctionData({
-                abi: USDC_ABI,
+                abi,
                 functionName: 'transfer',
                 args: [feeWalletAddress, feeInWei],
             });
