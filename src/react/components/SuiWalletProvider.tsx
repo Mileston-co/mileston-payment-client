50a232b5d2961fa56982df23fd6b93c325121d3a
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-05-03 14:50:52 +0100

    Update .npmignore and package.json, and export SuiWalletProvider component

--- src/react/components/SuiWalletProvider.tsx
+++ src/react/components/SuiWalletProvider.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
 import { getFullnodeUrl } from '@mysten/sui/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '@mysten/dapp-kit/dist/index.css';
 
 
 // Config options for the networks you want to connect to
 const { networkConfig } = createNetworkConfig({
-    localnet: { url: getFullnodeUrl('testnet') },
+    testnet: { url: getFullnodeUrl('testnet') },
     mainnet: { url: getFullnodeUrl('mainnet') },
 });
 const queryClient = new QueryClient();
@@ -15,7 +16,7 @@ const queryClient = new QueryClient();
 export default function SuiWalletProvider({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
-            <SuiClientProvider networks={networkConfig} defaultNetwork="localnet">
+            <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
                 <WalletProvider autoConnect={true}>
                     {children}
                 </WalletProvider>
